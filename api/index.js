6332af39a0e54e8280ff5cc44448dd6c027e8642
dkdldkdlf <dkdldkdlf@naver.com>
2025-08-15 20:48:00 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,18 +3,18 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   // 환경 변수에서 LINE 토큰을 가져옵니다.
-  const lineToken = process.env.LINE_ACCESS_TOKEN;
+  const lineToken = 'QLEVOPZpqpO8SptudJJ03W/Rz/YP5o3GZnABZ+A2hXgz1SVo5RRII0/aEW9SL55l/qbA15p2MivU1YdGtZ/aEyURqbN+vXiNSDje2b0eqIpGapcSjbjNiiE5s/qXgdRy/1jqXf6yFrMIGLWlKspRrQdB04t89/1O/w1cDnyilFU=';
 
   // ESP32에서 보낼 메시지 내용을 정의합니다.
   const message = 'ESP32에서 알림이 왔습니다!';
 
   // 푸시 메시지를 받을 LINE 사용자 ID 또는 그룹 ID를 여기에 입력하세요.
   // 이 정보는 LINE Developers 콘솔에서 찾을 수 있습니다.
-  const lineUserID = 'YOUR_LINE_USER_ID_OR_GROUP_ID';
+  const lineUserID = 'U326ad71508358143c1673f43f39d0ebb';
 
   try {
     await axios.post(
-      'https://api.line.me/v2/bot/message/push',
+      'https://api.line.me/v2w',
       {
         to: lineUserID,
         messages: [{
